b3df34be53e5b6975e930965d7940b8ed5259dc0
Nick Richmond <nwrichmond@turnt.tech>
2019-12-20 09:41:00 -0500

    Move Smile script handling to plugin.js

--- lib/components/SmileWidget.vue
+++ lib/components/SmileWidget.vue
@@ -1,51 +1,40 @@
 <template>
   <div>
     <client-only>
-      <div
-        class="sweettooth-init"
-        :data-channel-api-key="smileApiKey"
-        v-bind="customerData"
-      />
+      <div class="sweettooth-init" :data-channel-api-key="smileApiKey" v-bind="customerData" />
       <div class="sweettooth-launcher" />
-      <script async src="https://cdn.sweettooth.io/assets/storefront.js" charset="utf-8" />
     </client-only>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       customerData: {}
     }
   },
   computed: {
-    smileApiKey () {
-      if (
-        this.$nacelle &&
-        this.$nacelle.smileKey
-      ) {
+    smileApiKey() {
+      if (this.$nacelle && this.$nacelle.smileKey) {
         return this.$nacelle.smileKey
       }
 
       return ''
     }
   },
-  mounted () {
+  mounted() {
     const smileCookie = this.getCookieValue('nacelle_smile_user')
-    const smileData = smileCookie.length > 0 ? JSON.parse(smileCookie) : undefined
+    const smileData =
+      smileCookie.length > 0 ? JSON.parse(smileCookie) : undefined
 
-    if (
-      smileData &&
-      smileData.digest &&
-      smileData.digest.length > 0
-    ) {
+    if (smileData && smileData.digest && smileData.digest.length > 0) {
       this.customerData['data-external-customer-id'] = smileData.customerId
       this.customerData['data-customer-auth-digest'] = smileData.digest
     }
   },
   methods: {
-    getCookieValue (a) {
+    getCookieValue(a) {
       const b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)')
       return b ? decodeURIComponent(b.pop()) : ''
     }
